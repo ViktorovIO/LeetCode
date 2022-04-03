8d548d706eae614d64d4c6ccfd216ccc793edec0
ViktorovIO <viktorov_igor_1993@mail.ru>
2022-04-03 14:08:29 +0500

    Modify RomanToInteger solution on PHP

--- src/Problems/Easy/RomanToInteger/Solution.php
+++ src/Problems/Easy/RomanToInteger/Solution.php
@@ -9,12 +9,21 @@ class Solution
      */
     function romanToInt($s) {
         $result = 0;
+        $mapSymbolArray = [
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+        ];
         $symbolsList = str_split($s);
         $lastIndex = count($symbolsList)-1;
 
         foreach ($symbolsList as $key => $symbol) {
-            $mappedSymbol = $this->mapSymbol($symbol);
-            if ($key < $lastIndex && $mappedSymbol < $this->mapSymbol($symbolsList[$key + 1])) {
+            $mappedSymbol = $mapSymbolArray[$symbol];
+            if ($key < $lastIndex && $mappedSymbol < $mapSymbolArray[$symbolsList[$key + 1]]) {
                 $result -= $mappedSymbol;
             } else {
                 $result += $mappedSymbol;
@@ -22,24 +31,5 @@ class Solution
         }
 
         return $result;
-    }
-
-    function mapSymbol($symbol) {
-        switch ($symbol) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-        }
     }
 }
