1d1363c3a86abe265c2957799625e3a4586058d9
ViktorovIO <viktorov_igor_1993@mail.ru>
2023-02-10 12:31:13 +0500

    14. Longest Common Prefix

---
+++ src/Problems/Easy/LongestCommonPrefix/Solution.php
@@ -0,0 +1,31 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix($strs) {
+        $strsCount = count($strs);
+        if ($strsCount < 2) {
+            return $strs ? $strs[0] : "";
+        }
+
+        $tempPrefix = $strs[0];
+
+        for ($i = 0; $i < $strsCount; $i++) {
+            for ($j = 0; $j < strlen($tempPrefix); $j++) {
+                if (isset($strs[$i][$j]) && $tempPrefix[$j] === $strs[$i][$j]) {
+                    $tempPrefix[$j] = $strs[$i][$j];
+                } else {
+                    break;
+                }
+            }
+
+            $tempPrefix = substr($tempPrefix, 0, $j);
+        }
+
+        return $tempPrefix;
+    }
+}
